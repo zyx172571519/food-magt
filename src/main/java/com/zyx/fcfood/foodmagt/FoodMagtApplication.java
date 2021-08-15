dd664170d2e804c3c329e8cdfd26e9bec13f72ba
zyx172 <zyx172571@163.com>
2021-08-15 17:16:13 +0800

    Initial commit

---
+++ src/main/java/com/zyx/fcfood/foodmagt/FoodMagtApplication.java
@@ -0,0 +1,12 @@
+package com.zyx.fcfood.foodmagt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMagtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodMagtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyx/fcfood/foodmagt/FoodMagtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyx.fcfood.foodmagt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMagtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
